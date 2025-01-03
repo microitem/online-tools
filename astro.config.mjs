f18326061f6e8a217cbb60bdcbca7be5a6e79f0e
Peter Divéky <info@microitem.com>
2025-01-03 14:20:35 +0100

    Add: Basic project configuration and structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://goodboog.com',
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          sk: 'sk-SK',
+          de: 'de-DE',
+          fr: 'fr-FR',
+          es: 'es-ES'
+        }
+      }
+    })
+  ],
+  output: 'static',
+  build: {
+    inlineStylesheets: 'always'
+  },
+  compressHTML: true
+});
